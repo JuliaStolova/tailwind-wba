ee2c4cda23094313072b3663f743ae18b3f1ce71
Ignis Reginae <julia.stolova112@gmail.com>
2025-05-14 18:29:09 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-primary text-2xl font-bold">Welcome to SvelteKit</h1>
+<p class="text-secondary">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
